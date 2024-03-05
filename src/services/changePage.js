ca29764e89039fa0e5c01003d7c2514b8a8b697b
KikoUguina <kipuguina@gmail.com>
2024-03-05 13:26:14 +0100

    font & icomoon & services

---
+++ src/services/changePage.js
@@ -0,0 +1,46 @@
+import { animateOnScroll } from './animate-on-scroll'
+
+export function changePage(page) {
+    // Eliminar la clase 'selectedLi' de todos los elementos
+    const menuItems = document.querySelectorAll('header nav ul li a');
+    menuItems.forEach(item => {
+        item.classList.remove('selectedLi');
+    });
+
+    // Seleccionar la página actual y actualizar el título
+    switch (page) {
+        case 'home':
+            document.title = "ElaDelReves - Home";
+            break;
+        case 'laela':
+            document.title = "ElaDelReves - La Ela";
+            break;
+        case 'blog':
+            document.title = "ElaDelReves - Blog";
+            break;
+        case 'delreves':
+            document.title = "ElaDelReves - Del Revés";
+            break;
+        case 'unete':
+            document.title = "ElaDelReves - Únete";
+            break;
+        case 'login':
+            document.title = "ElaDelReves - Login";
+            break;
+        case 'register':
+            document.title = "ElaDelReves - Register";
+            break;
+        // Agrega más casos según las páginas que tengas
+        default:
+            break;
+    }
+    
+    animateOnScroll();
+    window.scrollTo(0, 0);
+
+    // Agregar la clase 'selectedLi' al elemento correspondiente a la página actual
+    const currentPageElement = document.getElementById(page);
+    if (currentPageElement) {
+        currentPageElement.classList.add('selectedLi');
+    }
+}
