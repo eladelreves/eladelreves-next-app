c1a339c0ab61598cee6da8d8f71c7e7e087190d3
KikoUguina <kipuguina@gmail.com>
2024-05-12 21:22:55 +0200

    Upload Video, not lazy loading yet

---
+++ src/app/delreves/video/Video.jsx
@@ -0,0 +1,20 @@
+import styles from './video.module.css'
+import PropTypes from 'prop-types';
+
+Video.propTypes = {
+    user: PropTypes.string,
+    video: PropTypes.string
+};
+
+export function Video({ user, video }){
+    return(
+        <div className={styles.div_con_video}>
+            <span>{user}</span>
+
+            <video autoPlay loop controls>
+                <source src={video} type="video/mp4"/>
+                Tu navegador no admite el elemento de video.
+            </video>
+        </div>
+    )
+}
