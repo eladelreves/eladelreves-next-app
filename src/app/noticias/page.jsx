cc4b1823d3e2781fc9f6a98af0012db465421f51
marionador <marionetiofficial@gmail.com>
2024-05-29 19:25:46 +0200

    test3

--- src/app/noticias/page.jsx
+++ src/app/noticias/page.jsx
@@ -4,16 +4,16 @@ import styles from './noticias.module.css';
 import { Suspense } from 'react';
 import GridContainerSkeleton from '../../components/news/gridContainer/GridContainerSkeleton';
 
-// export default function Noticias() {
-//     return (
-//         <>
-//             <h2 id={styles.h2}>¿Qué está <span className="elaGreen">pasando</span>?</h2>
+export default function Noticias() {
+    return (
+        <>
+            <h2 id={styles.h2}>¿Qué está <span className="elaGreen">pasando</span>?</h2>
 
-//             <AddNewForm></AddNewForm>
+            <AddNewForm></AddNewForm>
 
-//             <Suspense fallback={<GridContainerSkeleton />}>
-//                 <GridContainer></GridContainer>
-//             </Suspense>
-//         </>
-//     );
-// }
+            <Suspense fallback={<GridContainerSkeleton />}>
+                <GridContainer></GridContainer>
+            </Suspense>
+        </>
+    );
+}
