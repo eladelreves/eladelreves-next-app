8d4b5524d6889d2d1f8ad7c8bef7ee13c97b0054
marionador <marionetiofficial@gmail.com>
2024-05-29 20:14:42 +0200

    CreatedAt fixed

--- src/components/_templates/newCard/NewCard2.jsx
+++ src/components/_templates/newCard/NewCard2.jsx
@@ -1,15 +1,24 @@
 'use client'
+import { format } from 'date-fns';
 import styles from './newCard2.module.css'
+import { es } from 'date-fns/locale';
 import Link from 'next/link';
 
-export default function NewCard2({ gridClassName, id, title, createdAt, images }) {
+export default function NewCard2({ gridClassName, data }) {
+    const formatDate = (timestamp) => {
+        const milliseconds = timestamp.seconds * 1000 + Math.floor(timestamp.nanoseconds / 1e6);
+        const date = new Date(milliseconds);
+        const formattedDate = format(date, "d 'de' MMMM 'de' yyyy", { locale: es });
+        return formattedDate;
+    };
+
     return (
         <>
-            {id &&
-                <Link href={'/noticias/detalle?id=' + id} className={`${styles.news_card} ${gridClassName}`} style={{ backgroundImage: `url(${images[0]})` }}>
+            {data &&
+                <Link href={'/noticias/detalle?id=' + data.id} className={`${styles.news_card} ${gridClassName}`} style={{ backgroundImage: `url(${data.images[0]})` }}>
                     <div className={styles.news_info}>
-                        <span>{createdAt}</span>
-                        <h3>{title}</h3>
+                        <span>{formatDate(data.createdAt)}</span>
+                        <h3>{data.title}</h3>
                     </div>
                 </Link>
             }
--- src/components/news/gridContainer/GridContainer.jsx
+++ src/components/news/gridContainer/GridContainer.jsx
@@ -1,19 +1,9 @@
-
 import NewCard2 from '@components/_templates/newCard/NewCard2';
 import styles from './gridContainer.module.css';
 import { getLatestNews } from '@services/News';
-import { format } from 'path';
-import { es } from 'date-fns/locale';
 
 export default async function GridContainer() {
     let newsData = await getLatestNews();
-
-    const formatDate = (timestamp) => {
-        const milliseconds = timestamp.seconds * 1000 + Math.floor(timestamp.nanoseconds / 1e6);
-        const date = new Date(milliseconds);
-        const formattedDate = format(date, "d 'de' MMMM 'de' yyyy", { locale: es });
-        return formattedDate;
-    };
 
     return (
         <div id={styles.grid_container}>
@@ -21,10 +11,7 @@ export default async function GridContainer() {
                 <NewCard2
                     key={index}
                     gridClassName={index === 0 ? styles.large : styles.small}
-                    title={newsItem.title}
-                    createdAt={formatDate(newsItem.createdAt)}
-                    images={newsItem.images}
-                    id={newsItem.id}
+                    data={newsItem}
                 />
             ))}
         </div>
